0726ea0eca1d539f85dc71f76a3d8530d1a05d0f
wesleycosta13 <wesleyevil2016@gmai.com>
2024-08-08 16:07:15 -0300

    atualizando js

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
+document.addEventListener('DOMContentLoaded', () => {
+    const link1 = document.getElementById("link1");
+    const link2 = document.getElementById("link2");
+    const link3 = document.getElementById("link3");
+
+    if (link1) {
+        link1.addEventListener('click', () => {
+            scrollToElement('#Home');
+        });
+    }
+
+    if (link2) {
+        link2.addEventListener('click', () => {
+            scrollToElement('#Skills');
+        });
+    }
+
+    if (link3) {
+        link3.addEventListener('click', () => {
+            scrollToElement('#My Projects');
+        });
+    }
+});
+
+function scrollToElement(selector) {
+    const element = document.querySelector(selector);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
     }
 }
 
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav a');
 
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); // Previne o comportamento padrão de navegação
+
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
 
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
-});
-
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
-});
